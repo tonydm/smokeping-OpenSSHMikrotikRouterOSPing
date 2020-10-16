0aa488de29579dd4aadbf3016ec3b6404b935071
Tony DeMatteis <tonydema@gmail.com>
2020-10-16 10:43:38 -0700

    Added support for SSH Multiplexing and specifying a routing table

--- OpenSSHMikrotikRouterOSPing.pm
+++ OpenSSHMikrotikRouterOSPing.pm
@@ -20,8 +20,21 @@ use base qw(Smokeping::probes::basefork);
 use Net::OpenSSH;
 use Carp;
 
-# Debugging
+# # Debugging
 # use Data::Dumper;
+
+# use Log::Log4perl qw(:easy);
+# Log::Log4perl->easy_init(
+#   {
+#     file  => ">> /tmp/openssh_error_log",
+#     level => $ERROR,
+#   },
+#   {
+#     file  => "STDERR",
+#     level => $DEBUG,
+#   }
+# );
+# DEBUG("Debugging enabled...\n");
 
 my $e = "=";
 sub pod_hash {
@@ -32,7 +45,8 @@ DOC
   description => <<DOC,
 Connect to Mikrotik RouterOS Device via OpenSSH to run ping commands.
 This probe uses the "ping" cli of the Mikrotik RouterOS.  You have
-the option to specify which interface the ping is sourced from as well.
+the options to specify which interface the ping is sourced from, which
+routing table to use and multiplexd ssh connections.
 DOC
   notes => <<DOC,
 ${e}head2 Mikrotik RouterOS configuration
@@ -40,11 +54,11 @@ ${e}head2 Mikrotik RouterOS configuration
 The Mikrotik RouterOS device should have a username/password configured, and
 the ssh server must not be disabled.
 
-Make sure to connect to the remote host once from the commmand line as the
+Make sure to connect to the remote host once from the command line as the
 user who is running smokeping. On the first connect ssh will ask to add the
 new host to its known_hosts file. This will not happen automatically so the
-script will fail to login until the ssh key of your Mikrotik RouterOS box is in the
-known_hosts file.
+script will fail to login until the ssh key of your Mikrotik RouterOS device
+is in the known_hosts file.
 
 ${e}head2 Requirements
 
@@ -56,6 +70,10 @@ Tony DeMatteis E<lt>tonydema@gmail.comE<gt>
 based on L<Smokeping::Probes::OpenSSHJunOSPing> by Tobias Oetiker E<lt>tobi@oetiker.chE<gt>,
 which itself is
 based on L<Smokeping::probes::TelnetJunOSPing> by S H A N E<lt>shanali@yahoo.comE<gt>.
+
+Additional Credits:
+  Routing Table option - https://github.com/leostereo  Leandro contribuited suggestions and code
+  to be able to specify a specific routing table.  Thank you
 DOC
   }
 }
@@ -79,70 +97,148 @@ sub ProbeDesc($){
 sub pingone ($$){
   my $self = shift;
   my $target = shift;
+  my $host = $target->{vars}{source};
   my $port = $target->{vars}{ssh_port};
-  my $source = $target->{vars}{source};
-  my $dest = $target->{vars}{host};
-  my $psource = $target->{vars}{psource};
-  my @output = ();
   my $login = $target->{vars}{routerosuser};
   my $password = $target->{vars}{routerospass};
+  my $dest = $target->{vars}{host};
+  my $psource = $target->{vars}{psource};
   my $bytes = $self->{properties}{packetsize};
-  my $pings = $self->pings($target);
+  my $rtable = $self->{vars}{rtable};
   my $ssh_cmd = $target->{vars}{ssh_binary_path};
+  my $multiplex_ssh = $target->{vars}{multiplex_ssh};
+  my $multiplex_control_persist_time = $target->{vars}{multiplex_control_persist_time};
+  my $multiplex_control_socket_path = $target->{vars}{multiplex_control_socket_path};
+  my $pings = $self->pings($target);
+
+  # Try and determine script user in order to determine /home dir location for
+  # Master Control Socket file
+  my $script_user = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
+  my $script_user_home_dir = (getpwuid $>)[7];
+
+  # TODO (Add automatic home dir, tests, and fallback dir)
+  my $master_control_socket_dir = $multiplex_control_socket_path ? $multiplex_control_socket_path : "$script_user_home_dir/.libnet-openssh-perl";
+  my $master_control_socket_file = 'control-smokeping@' . $host;
+  my $master_control_socket_path_file = "$master_control_socket_dir/$master_control_socket_file";
+  $multiplex_control_persist_time = $multiplex_control_persist_time ? "$multiplex_control_persist_time" . "m" : ();
 
   # do NOT call superclass ... the ping method MUST be overwriten
   my %upd;
   my @args = ();
 
-  # Note: To debug the SSH Connection modify the master_opts options to include ""-vvv"
-  # master_opts => [-o => "StrictHostKeyChecking=no", "-vvv"],
-  my $ssh = Net::OpenSSH->new(
-    $source,
-    $login ? ( user => $login ) : (),
-    $password ? ( password => $password ) : (),
-    port => $port,
-    timeout => 60,
-    strict_mode => 0,
-    kill_ssh_on_timeout => 1,
-    ctl_dir => "/tmp/.libnet-openssh-perl",
-    master_opts => [-o => "StrictHostKeyChecking=no"],
-    ssh_cmd => $ssh_cmd
+  #  Define the base SSH connection options to pass to the Net::OpenSSH->new() connection method
+  my %opts = (
+    "user" => $login ? $login : (),
+    "password" => $password ? $password : (),
+    "port" => $port,
+    "timeout" => 60,
+    "strict_mode" => 0,
+    "ssh_cmd" => $ssh_cmd
+  );
+
+  # # DEBUG
+  # $Net::OpenSSH::debug = -1;
+
+  my $ssh;
+
+  # If multiplex_ssh connection is true/1, add necessary additional params to options hash
+  if ( $multiplex_ssh ) {
+    # $self->do_log("Using OpenSSH ControlMaster Multiplex connections!\n");
+    if(-e $master_control_socket_path_file){
+      # If a multiplex connection socket has already been created, use it
+      # $self->do_log("Master Control Socket file: $master_control_socket_path_file exists... Using.\n");
+
+      # Append options hash to use existing multiplex control socket
+      $opts{'external_master'} = 1;
+      $opts{'ctl_path'} = $master_control_socket_path_file;
+    } else {
+      # No multiplex connection socket has been created for this host, so create one
+      # $self->do_log("Master Control Socket file: $master_control_socket_path_file does not exist!  Creating new socket file.\n");
+
+      # Append options hash to create a multiplex control socket
+      $opts{'ctl_dir'} = $master_control_socket_dir;
+      $opts{'ctl_path'} = $master_control_socket_path_file;
+      $opts{'master_opts'} = ["-oStrictHostKeyChecking=no", "-oControlPersist=$multiplex_control_persist_time", "-vvv"];
+    }
+  } else {
+    # $self->do_log("Not using OpenSSH ControlMaster Multiplex connections!\n");
+  }
+
+  # # Debug - Show Options Hash
+  # $self->do_log(Dumper \%opts);
+
+  # Connect to source host
+  $ssh = Net::OpenSSH->new(
+    $host, %opts
   );
 
   if ($ssh->error) {
-    $self->do_log( "OpenSSHMikrotikRouterOSPing connecting $source: ".$ssh->error );
+    $self->do_log( "OpenSSHMikrotikRouterOSPing connecting $host: ".$ssh->error );
     return ();
   };
 
-  # Debug
+  # # Debug
   # $self->do_log("ping $dest count=$pings size=$bytes src-address=$psource");
 
+  # Build ping command
+  my $ping_command = "ping $dest";
+
   if ( $psource ) {
-    @output = $ssh->capture("ping $dest count=$pings size=$bytes src-address=$psource");
-  } else {
-    @output = $ssh->capture("ping $dest count=$pings size=$bytes");
+    $ping_command .= " src-address=$psource";
   }
 
+  if ( $pings ) {
+    $ping_command .= " count=$pings";
+  }
+
+  if ( $bytes ) {
+    $ping_command .= " size=$bytes";
+  }
+
+  if ( $rtable ) {
+    $ping_command .= " routing-table=$rtable";
+  }
+
+  $ping_command .= "\n";
+
+  # Execute the ping command on the source/host and capture the response
+  my @output = ();
+  @output = $ssh->capture($ping_command);
+
   if ($ssh->error) {
-    $self->do_log( "OpenSSHMikrotikRouterOSPing running commands on $source: ".$ssh->error );
+    $self->do_log( "OpenSSHMikrotikRouterOSPing running commands on $host: ".$ssh->error );
     return ();
   };
 
-  # Debug
+  # # Debug
+  # $self->do_log('========== Ping response ==========' . "\n");
   # $self->do_log(Dumper \@output);
 
+  # Process the ping response
   my @times = ();
 
   while (@output) {
     my $outputline = shift @output;
     chomp($outputline);
-    next if ($outputline =~ m/(sent|recieved|packet\-loss|min\-rtt|avg\-rtt)/);
+    next if ($outputline =~ m/(sent|recieved|packet\-loss|min\-rtt|avg\-rtt|max\-rtt)/);
     $outputline =~ /(\d+)ms/ && push(@times,$1);
   }
 
+  # Convert the ping times values to RRD format
   @times = map {sprintf "%.10e", $_ / $self->{pingfactor}} sort {$a <=> $b} @times;
+  
+  # Ensure the number of pings requested returned by @tumes are equal to the
+  # configured number of pings defined in the host definition.  Any value
+  # other than the number defined in the RRD format will cause the update
+  # to fail
+  my $length = @times;
+  while (($length = @times) > 20) {
+    $self->do_log('@times length: ' . $length . "\n");
+    pop @times;
+  }
 
-  # Debug
+  # # Debug
+  # $self->do_log('@times:');
   # $self->do_log(Dumper \@times);
   # my $length = @times;
   # $self->do_log("Length of times: $length");
@@ -150,6 +246,7 @@ sub pingone ($$){
   return @times;
 }
 
+# Params defined - param name, default value, eval allowed value, documentation
 sub probevars {
   my $class = shift;
   return $class->_makevars($class->SUPER::probevars, {
@@ -175,10 +272,6 @@ DOC
 
 sub targetvars {
   my $class = shift;
-  # Override defaults
-  # Modify pings
-  # Add ssh_port
-  # Add ssh_binary_path
   my $h = $class->SUPER::targetvars;
   delete $h->{pings};
 
@@ -209,17 +302,23 @@ DOC
     },
     routerosuser => {
       _doc => <<DOC,
-The routerosuser option allows you to specify a username that has ping
-capability on the Mikrotik RouterOS Device.
+The routerosuser option allows you to specify the SSH login username and 
+that has ping capability on the Mikrotik RouterOS Device.
 DOC
       _example => 'user',
     },
     routerospass => {
       _doc => <<DOC,
-The routerospass option allows you to specify the password for the username
-specified with the option routerosuser.
+The routerospass option allows you to specify the SSH login password.
 DOC
       _example => 'password',
+    },
+    rtable => {
+      _doc => <<DOC,
+The (optional) rtable option lets you specify the routing table to use in the
+ping command.
+DOC
+    _default => ''
     },
     pings => {
       _doc => <<DOC,
@@ -238,20 +337,59 @@ DOC
       _example => "20",
     },
     ssh_port => {
-      _doc => 'Connect to this port.',
+      _doc => <<DOC,
+The (optional) ssh_port option lets you specify a non standard SSH port.
+DOC
       _re => '\d+',
       _default => 22,
       _example => 22431,
     },
     ssh_binary_path => {
       _doc => <<DOC,
-The ssh_binary_path option specifies the path for the ssh client binary.
-This option will specify the path to the OpenSSH host connector.  It may be
-necessary to define the path to the binary if it is not found.  To find the
-path use "which ssh".  Default: /usr/bin/ssh
+The (optional) ssh_binary_path option lets you specify the path for the ssh client binary.
+This option will specify the path to the Net::OpenSSH host connector.  It may be
+necessary to define the path to the binary if it is not found in the \$PATH.
 DOC
       _default => "/usr/bin/ssh",
       _example => "/usr/bin/ssh",
+    },
+    multiplex_ssh => {
+      _doc => <<DOC,
+The (optional) multiplex_ssh option lets you configure Net::OpenSSH to use multiplexed
+ssh connections.  i.e. reuse the same SSH connection to a host.  Default is enabled (=1)
+DOC
+      _default => 1, # True
+      _re => '\d+',
+      _sub => sub {
+        my $val = shift;
+        return "ERROR: multiplex_ssh value of $val is invalid.  Must be 0 for false, 1 for true"
+          unless $val == 0 or $val == 1;
+        return undef;
+      },
+      _example => 1
+    },
+    multiplex_control_persist_time => {
+      _doc => <<DOC,
+The (optional) multiplex_control_persist_time ssh option lets you configure how
+long to persist the multiplex or Master Control Socket file 
+DOC
+      _default => 10, # 10 Min
+      _re => '\d+',
+      _sub => sub {
+        my $val = shift;
+        return "ERROR: multiplex_control_persist_time value of $val is invalid.  Must be >= 1 and <= 1000"
+          unless $val >= 1 and $val <= 1000;
+        return undef;
+      },
+      _example => "10"
+    },
+    multiplex_control_socket_path => {
+      _doc => <<DOC,
+The (optional) multiplex_control_persist_time ssh option lets you configure how
+long to persist the multiplex or Master Control Socket file 
+DOC
+      _default => "~/.libnet-openssh-perl",
+      _example => "/tmp/smokeping_ssh_sockets"
     }
   };
 
--- README.md
+++ README.md
@@ -25,61 +25,113 @@ I wanted a probe to connect to Mikrotik RouterOS devices via SSH. So I created t
 [![GitHub stars](https://img.shields.io/github/stars/tonydm/smokeping-OpenSSHMikrotikRouterOSPing)](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/stargazers)
 [![GitHub issues](https://img.shields.io/github/issues/tonydm/smokeping-OpenSSHMikrotikRouterOSPing)](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/issues)
 
-
 ### Requirements
- - Net::OpenSSH
- - IO::Pty
 
- ** Your OS may require that you install the openssh-client if you are seeing any of the following errors:
-   - ```SSH connection failed: unable to establish master SSH connection: bad password or master process exited unexpectedly at```
-   -  ```unable to establish master SSH connection```
+- Net::OpenSSH
 
- This is because the version of the Net::OpenSSH Perl module installed on your distro does not provide the multiplexing functionality required
-
+- IO::Pty
+  
+  ** Your OS may require that you install the openssh-client if you are seeing any of the following errors:
+  
+  - ```SSH connection failed: unable to establish master SSH connection: bad password or master process exited unexpectedly at```
+  - ```unable to establish master SSH connection```
+  
+  This is because the version of the Net::OpenSSH Perl module installed on your distro does not provide the multiplexing functionality required
+  
   According to Net::OpenSSH [documentation](https://metacpan.org/pod/Net::OpenSSH#Solaris-(and-AIX-and-probably-others)):
-
-   - To install the needed support
-     - Alpine dist - run: apk add openssh-client
-     - debian dist - run: [sudo] apt install openssh-client
+  
+  - To install the needed support
+    - Alpine dist - run: apk add openssh-client
+    - debian dist - run: [sudo] apt install openssh-client
 
 ### Supports
- - Source IP
- - Host (dest) IP or FQDN.  (FQDN if DNS is enabled on Mikrotik Router)
- - Packet Size (Default: 56)
- - Target Ping Count (Default: 20, MAX: 50)
- - Target SSH Port (Default: 22)
- - User defined openssh-client path (/usr/bin/ssh)
+
+- Source IP
+- Host (dest) IP or FQDN.  (FQDN if DNS is enabled on Mikrotik Router)
+- Packet Size (Default: 56)
+- Target Ping Count (Default: 20, MAX: 50)
+- Specify Routing Table
+- Target Host SSH Port (Default: 22)
+- User defined openssh-client path (/usr/bin/ssh)
+- Multiplexed SSH Connections (Enabled by default)
+  - User defined SSH Control Socket File Path (Default: ~/.libnet-openssh-perl)
+  - User defined SSH Control Socket Persist Timeout (Default: 10 min)
+
+
+### Multiplexed SSH Connections
+
+Multiplexing is the ability to send more than one signal over a single line or connection. In OpenSSH (>=v3.9), multiplexing can re-use an existing outgoing TCP connection for multiple concurrent SSH sessions to a remote SSH server, avoiding the overhead of creating a new TCP connection (on both the local and remote hosts) and reauthenticating each time.  Reference: [https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Multiplexing](https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Multiplexing#:~:text=Multiplexing%20is%20the%20ability%20to,connection%20and%20reauthenticating%20each%20time)
+
+There are some requirements for this feature to work.  OpenSSH requires that the directory and it's parents, where the Master Control Socket File is created, must be writable only by the current effective user or root, otherwise the connection will be aborted to avoid insecure operation.  By default ~/.libnet-openssh-perl is used.
+
+This probe will attempt to determine the $HOME directory of the user running/executing Smokeping, usually "smokeping" or "root".  In some cases, if using Docker or other container platform, the user could be "abc" in the case of using s6-supervise for example.  You can override this behaviour and specify the directory where the Master Control Socket File is created by setting the multiplex_socket_file_path option in the Probes config file.  You must ensure that the path meets the requirements as previously stated and that the permission masks be 0755 or more restrictive so that no other user can write to the dir/file.
+
+  - The error you will see in the smokeping.log (if enabled) if you have defined your own socket file path w/o properly setting up permissions:
+
+  - ```OpenSSHMikrotikRouterOSPing: OpenSSHMikrotikRouterOSPing connecting 10.10.0.1: unable to establish master SSH connection: bad password or master process exited unexpectedly```
+
+
+For each unique source (router) the probe will create a unique master control socket file.
+
+
+#### Example of a user defined path (using Docker w/ s6-supervise)
+##### Note: smokeping is run as user abc in this scenario
+
+```
+abc@5fef45006a04: mkdir /tmp/smokeping_ssh_sockets
+abc@5fef45006a04: chown -R abc:users /tmp/smokeping_ssh_sockets
+abc@5fef45006a04: chmod -R 0744 /tmp/smokeping_ssh_sockets
+```
+
+Two multiplexed control sockets have been created for two target hosts
+```
+abc@5fef45006a04:/$ ls -alF /tmp/smokeping_ssh_sockets/
+total 8
+drwxr--r-- 2 abc  users 4096 Oct 16 08:55  ./
+drwxrwxrwt 1 root root  4096 Oct 16 08:47  ../
+srw------- 1 abc  users    0 Oct 16 08:55 'control-smokeping@10.10.0.1'=
+srw------- 1 abc  users    0 Oct 16 08:55 'control-smokeping@159.118.128.200'=
+
+```
+
+See https://metacpan.org/pod/Net::OpenSSH for full documentation
+
 
 ## Setup
 
 ### Mikrotik Config
- - Create a smokeping group with limited policy rights (ssh, read, test).  Then create a smokeping user with the smokeping group rights
 
- ![User](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-users.png)
-
+- Create a smokeping group with limited policy rights (ssh, read, test).  Then create a smokeping user with the smokeping group rights
+  
+  ![User](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-users.png)
+  
   ![User2](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-users2.png)
 
- - Ensure you have the SSH service enabled.
+- Ensure you have the SSH service enabled.
+  
+  ![Service1](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-services-settings1.png)
 
- ![Service1](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-services-settings1.png)
-
- - Set/Filter the allowed IPs either in the IP->Services or set up a Firewall rule to permit SSH connections per your security policy
-
- ![Service2](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-services-settings2.png)
+- Set/Filter the allowed IPs either in the IP->Services or set up a Firewall rule to permit SSH connections per your security policy
+  
+  ![Service2](https://github.com/tonydm/smokeping-OpenSSHMikrotikRouterOSPing/blob/master/screenshots/winbox-services-settings2.png)
 
 ### Smokeping Config
- - Copy the OpenSSHMikrotikRouterOSPing.pm file to the Smokeping probes directory on your server.  It can be located in a number of places depending on your distro.
 
-    Common locations are:
-    - Ubuntu: /usr/share/perl5/Smokeping/probes
-    - Alpine docker: /usr/share/perl5/vendor_perl/Smokeping/probes
-    - Or 'sudo find / -type f -name Smokeping.pm' to locate
+- Copy the OpenSSHMikrotikRouterOSPing.pm file to the Smokeping probes directory on your server.  It can be located in a number of places depending on your distro.
+  
+   Common locations are:
+  
+  - Ubuntu: /usr/share/perl5/Smokeping/probes
+  - Alpine docker: /usr/share/perl5/vendor_perl/Smokeping/probes
+  - Or 'sudo find / -type f -name Smokeping.pm' to locate
 
+#### 
 
 #### Probes
 
 ###### /etc/smokeping/config.d/probes
-````
+
+```
 + OpenSSHMikrotikRouterOSPing
 
 forks = 5
@@ -91,37 +143,85 @@ pings = 20
 routerospass = <userpass>
 routerosuser = <username>
 ssh_binary_path = /usr/bin/ssh
-````
+multiplex_ssh = 1
+multiplex_control_persist_time = 10
+multiplex_control_file_path = <user defined path>
+rtable = <routing table name>
+```
+
 ** Note: Use "which ssh" to determine the ssh binary path
 
-### Targets
+### Targets (Sample Configs)
+
 ###### /etc/smokeping/config.d/Targets
 
-  ````
-+ Edgerouter
+```
+# Config Examples
 
++ Edgerouter
+# Define some defaults for this sections Targets 
+probe = OpenSSHMikrotikRouterOSPing
 title = Edge Router
 menu = Edge Router
-probe = OpenSSHMikrotikRouterOSPing
 source = 172.20.0.1
-psource = <WAN/Public Facing IP Address>
+psource = <WAN/Public Facing IP Address or other Internal Facing Interface or IP Gateway>
 
 ++ nyc1_digitalocean_com
 title = speedtest-nyc1.digitalocean.com (DigitalOcean New York 1)
 host = speedtest-nyc1.digitalocean.com
 # source - uses parent defined
 # psource - uses parent defined
+# multiplex_ssh = 1 # Default
+# multiplex_control_file_path = ~/.libnet-openssh-perl # Default
+# multiplex_control_persist_time = 10 # Default
 
-++ my_remote_router
-title = My Remote Router
-source = myremoterouter_IP_WAN_Address
-# psource - not used.  will use source IP
-host = my_edge_router_IP
+
+# More Config Examples
+
+++ RemoteRouters
+# Define some defaults for this sections Targets 
+probe = OpenSSHMikrotikRouterOSPing
+title = Remote Routers
+menu = Remote Routers
+
+++ remote_router1
+title = Remote Router1
+source = remoterouter1_WAN_IP_Address
+# psource - No default defined, will use source address to source pings
+host = IP_of_interest
 ssh_port = 22431
-  ````
+
+++ remote_router2
+title = Remote Router2
+source = remoterouter2_WAN_IP_Address
+psource = some_other_IP_address_on_remote_router
+host = IP_of_interest
+ssh_port = 29437
+multiplex_ssh = 0 # Don't use multiplexed ssh connections - but why would you want to
+
+++ remote_router3
+title = Remote Router3
+source = remoterouter3_WAN_IP_Address
+host = IP_of_interest
+ssh_port = 29437
+# multiplex_ssh = 1 # Default behaviour
+multiplex_control_file_path = /tmp/smokeping_ssh_sockets # Override default ~/.libnet-openssh-perl
+multiplex_control_persist_time = 20 # Override to use 20 minutes
+rtable = name_of_routing_table_other_than_main
+
+```
 
 ### Bugs
-  - None reported
+
+- None reported
+
+### TODO
+
+- Add support 
+  - SSH Key Authentication
+  - Specify Interface Name
+  - Specify DSCP
+  - Specify TTL
 
 ### License
 
